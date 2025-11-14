3277dbfbbcea9a0f1ff7a68b237bd4195782a44e
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-11-14 18:19:17 +0500

    configuration

--- lib/ui/widgets/textformfield.dart
+++ lib/ui/widgets/textformfield.dart
@@ -2,6 +2,7 @@ import 'package:app/ui/widgets/responsive_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+// ignore: must_be_immutable
 class CustomTextField extends StatelessWidget {
   final String hint;
   final TextEditingController textEditingController;
